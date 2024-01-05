e4039b0cfdd11388494cb6c27c43dde78fea98ff
sbanon <92295748+sb01101001@users.noreply.github.com>
2024-01-05 23:56:15 +1100

    Add logger rotation

--- logger.py
+++ logger.py
@@ -1,15 +1,19 @@
 import logging
-
+from logging.handlers import RotatingFileHandler
 
 def set_logger(name):
     logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
+
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    file_handler = logging.FileHandler('main.log')
+    file_handler = RotatingFileHandler('main.log', maxBytes=1024*1024*5, backupCount=3, delay=True)  # 5 MB per file, with 3 backups
     file_handler.setFormatter(formatter)
+
     stream_handler = logging.StreamHandler()
     stream_handler.setLevel(logging.INFO)
     stream_handler.setFormatter(logging.Formatter('%(levelname)s - %(message)s'))
+
     logger.addHandler(file_handler)
     logger.addHandler(stream_handler)
+
     return logger
