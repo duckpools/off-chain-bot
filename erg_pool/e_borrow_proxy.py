8e5736a3f3298dad92985d90d4f3a7cb311df698
sbanon <92295748+sb01101001@users.noreply.github.com>
2024-02-02 18:19:20 +1100

    Eip 001 changes

--- erg_pool/e_borrow_proxy.py
+++ erg_pool/e_borrow_proxy.py
@@ -3,7 +3,7 @@ import json
 from consts import TX_FEE, MAX_BORROW_TOKENS, SIG_USD_ID, ERG_USD_DEX_NFT, SIG_RSV_ID, ERG_RSV_DEX_NFT, MIN_BOX_VALUE, \
     DOUBLE_SPENDING_ATTEMPT, DEFAULT_BUFFER
 from helpers.job_helpers import job_processor, latest_pool_info
-from helpers.node_calls import tree_to_address, box_id_to_binary, sign_tx
+from helpers.node_calls import tree_to_address, box_id_to_binary, sign_tx, current_height
 from helpers.platform_functions import get_dex_box, get_parent_box, get_head_child, \
     get_pool_param_box
 from helpers.serializer import encode_int_tuple, encode_long, encode_long_pair
@@ -40,6 +40,7 @@ def process_borrow_proxy_box(pool, box, latest_tx, fee=TX_FEE):
     amount_to_borrow = int(box["additionalRegisters"]["R5"]["renderedValue"])
     final_borrowed = borrowed + amount_to_borrow
     pool_param_box = get_pool_param_box(pool["parameter"], pool["PARAMETER_NFT"])
+    net_height = current_height() - 20
 
     transaction_to_sign = \
         {
@@ -83,7 +84,7 @@ def process_borrow_proxy_box(pool, box, latest_tx, fee=TX_FEE):
                         "R6": box["additionalRegisters"]["R7"]["serializedValue"],
                         "R7": box["additionalRegisters"]["R8"]["serializedValue"],
                         "R8": box["additionalRegisters"]["R9"]["serializedValue"],
-                        "R9": encode_long_pair(int(box["additionalRegisters"]["R6"]["renderedValue"]) + pool["proxy_forced_liquidation"], DEFAULT_BUFFER)
+                        "R9": encode_long_pair(net_height + pool["proxy_forced_liquidation"], DEFAULT_BUFFER)
                     }
                 },
                 {
